4add7c4351c0783d537296a0c667867b4d228699
svnaditya <svn.aditya1997@gmail.com>
2026-07-22 00:01:44 +0530

    releasing the v2 version

--- car_kan.js
+++ car_kan.js
@@ -1,46 +1,50 @@
-// Kannada Keyboard with Vedic Accents (Custom Implementation)
+// Kannada Keyboard with Vedic Accents
+// Base transliteration engine adapted from Lexilogos (copyright lexilogos.com),
+// extended with Vedic accents, avagraha, danda and a clipboard helper.
 (function() {
     const textarea = document.getElementById("bar");
 
     const replacements = [
-        // 1. Independent Vowels
+        // === Vocalic R / L (ri, li) ===
+        [/್-r/g, "ೃ"],
+        [/-r/g, "ಋ"],
+        [/ಋi/g, "ೠ"],
+        [/ೃi/g, "ೄ"],
+
+        [/್-l/g, "ೢ"],
+        [/-l/g, "ಌ"],
+        [/ಌi/g, "ೡ"],
+        [/ೢi/g, "ೣ"],
+
+        // === Independent vowels ===
+        [/ಅa/g, "ಆ"],
+        [/ಅi/g, "ಐ"],
+        [/ಅu/g, "ಔ"],
         [/a/g, "ಅ"],
-        [/[AāĀ]/g, "ಆ"],
+        [/[Aāâ]/g, "ಆ"],
+        [/ಇi/g, "ಈ"],
         [/i/g, "ಇ"],
-        [/[IīĪ]/g, "ಈ"],
+        [/[Iīî]/g, "ಈ"],
+        [/ಉu/g, "ಊ"],
         [/u/g, "ಉ"],
-        [/[UūŪ]/g, "ಊ"],
-        [/ಅಅ/g, "ಆ"],
-        [/ಇಇ/g, "ಈ"],
-        [/ಉಉ/g, "ಊ"],
+        [/[Uūû]/g, "ಊ"],
+        [/ಎe/g, "ಏ"],
         [/e/g, "ಎ"],
-        [/ಎಎ/g, "ಏ"],
-        [/[EēĒ]/g, "ಏ"],
-        [/ai/g, "ಐ"],
-        [/ಅಇ/g, "ಐ"],
+        [/[Eēê]/g, "ಏ"],
+        [/ಒo/g, "ಓ"],
         [/o/g, "ಒ"],
-        [/ಒಒ/g, "ಓ"],
-        [/[OōŌ]/g, "ಓ"],
-        [/au/g, "ಔ"],
-        [/ಅಉ/g, "ಔ"],
+        [/[Oōô]/g, "ಓ"],
 
-        // 2. Virama (Halant) and Dependent Vowels (Matras)
-        // \u0CCD is the Kannada Virama (್)
-        // \u200b is the Zero Width Space used to hold the place for matras
-        [/್ಅ/g, "\u200b"],
-        [/\u200bಅ/g, ""], 
-        [/\u200bಆ/g, "ಾ"],
-        [/\u200bಇ/g, "ಿ"],
-        [/\u200bಈ/g, "ೀ"],
-        [/\u200bಉ/g, "ು"],
-        [/\u200bಊ/g, "ೂ"],
-        [/\u200bಎ/g, "ೆ"],
-        [/\u200bಏ/g, "ೇ"],
-        [/\u200bಐ/g, "ೈ"],
-        [/\u200bಒ/g, "ೊ"],
-        [/\u200bಓ/g, "ೋ"],
-        [/\u200bಔ/g, "ೌ"],
+        // === Virama removal / dependent vowels (matras) ===
+        [/ಿಇ/g, "ೀ"],
+        [/ುಉ/g, "ೂ"],
+        [/ೆಎ/g, "ೇ"],
+        [/ೊಒ/g, "ೋ"],
 
+        [/್ಅ/g, "​"],
+        [/​ಅ/g, "ಾ"],
+        [/​ಇ/g, "ೈ"],
+        [/​ಉ/g, "ೌ"],
         [/್ಆ/g, "ಾ"],
         [/್ಇ/g, "ಿ"],
         [/್ಈ/g, "ೀ"],
@@ -53,8 +57,10 @@
         [/್ಓ/g, "ೋ"],
         [/್ಔ/g, "ೌ"],
 
-// 3. Consonants
-        [/n/g, "ನ್"],
+        // === Consonants ===
+        [/ನ್g/g, "ಙ್"], // ng
+        [/[ṅG]/g, "ಙ್"],
+        [/[ñJ]/g, "ಞ್"],
         [/k/g, "ಕ್"],
         [/g/g, "ಗ್"],
         [/c/g, "ಚ್"],
@@ -64,23 +70,25 @@
         [/[NṇṆ]/g, "ಣ್"],
         [/t/g, "ತ್"],
         [/d/g, "ದ್"],
+        [/n/g, "ನ್"],
         [/p/g, "ಪ್"],
         [/b/g, "ಬ್"],
         [/m/g, "ಮ್"],
         [/y/g, "ಯ್"],
         [/r/g, "ರ್"],
         [/l/g, "ಲ್"],
-        [/[LḷḶ]/g, "ಳ್"],
+        [/[LḶḷ]/g, "ಳ್"],
         [/v/g, "ವ್"],
         [/w/g, "ವ್"],
         [/[SṣṢ]/g, "ಷ್"],
         [/s/g, "ಸ್"],
         [/[çzśŚ]/g, "ಶ್"],
-        [/[GṅṄ]/g, "ಙ್"],
-        [/[JñÑ]/g, "ಞ್"],
-        // Notice 'h' has been removed from here!
 
-        // 4. Aspirated Consonants
+        // === r and l variants (nukta forms) ===
+        [/ರ್=/g, "ಱ್"],
+        [/ಲ್=/g, "ೞ್"],
+
+        // === Aspirated consonants ===
         [/ಕ್h/g, "ಖ್"],
         [/ಗ್h/g, "ಘ್"],
         [/ಚ್h/g, "ಛ್"],
@@ -93,56 +101,63 @@
         [/ಬ್h/g, "ಭ್"],
         [/ಸ್h/g, "ಶ್"],
 
-        [/h/g, "ಹ್"], // 'h' is now safely at the end of the combinations!
+        [/h/g, "ಹ್"],
 
-        // Vocalic R / L
-        [/-r/g, "ಋ"],
-        [/ಋi/g, "ಋ"], 
-        [/್ಋ/g, "ೃ"],
-        [/-l/g, "ಌ"],
-        [/ಌi/g, "ಌ"], 
-        [/್ಌ/g, "ೢ"],
+        // === ZWS cleanup for consonants ===
+        [/​ಕ/g, "ಕ"], [/​ಖ/g, "ಖ"], [/​ಗ/g, "ಗ"], [/​ಘ/g, "ಘ"], [/​ಙ/g, "ಙ"],
+        [/​ಚ/g, "ಚ"], [/​ಛ/g, "ಛ"], [/​ಜ/g, "ಜ"], [/​ಝ/g, "ಝ"], [/​ಞ/g, "ಞ"],
+        [/​ಟ/g, "ಟ"], [/​ಠ/g, "ಠ"], [/​ಡ/g, "ಡ"], [/​ಢ/g, "ಢ"], [/​ಣ/g, "ಣ"],
+        [/​ತ/g, "ತ"], [/​ಥ/g, "ಥ"], [/​ದ/g, "ದ"], [/​ಧ/g, "ಧ"], [/​ನ/g, "ನ"],
+        [/​ಪ/g, "ಪ"], [/​ಫ/g, "ಫ"], [/​ಬ/g, "ಬ"], [/​ಭ/g, "ಭ"], [/​ಮ/g, "ಮ"],
+        [/​ಯ/g, "ಯ"], [/​ರ/g, "ರ"], [/​ಱ/g, "ಱ"], [/​ಲ/g, "ಲ"], [/​ಳ/g, "ಳ"],
+        [/​ವ/g, "ವ"], [/​ಹ/g, "ಹ"], [/​ಶ/g, "ಶ"], [/​ಷ/g, "ಷ"], [/​ಸ/g, "ಸ"],
+        [/​ೞ/g, "ೞ"], [/​ /g, " "],
 
-        // 5. Cleanup for basic consonants
-        [/\u200bಕ/g, "ಕ"], [/\u200bಖ/g, "ಖ"], [/\u200bಗ/g, "ಗ"], [/\u200bಘ/g, "ಘ"], [/\u200bಙ/g, "ಙ"],
-        [/\u200bಚ/g, "ಚ"], [/\u200bಛ/g, "ಛ"], [/\u200bಜ/g, "ಜ"], [/\u200bಝ/g, "ಝ"], [/\u200bಞ/g, "ಞ"],
-        [/\u200bಟ/g, "ಟ"], [/\u200bಠ/g, "ಠ"], [/\u200bಡ/g, "ಡ"], [/\u200bಢ/g, "ಢ"], [/\u200bಣ/g, "ಣ"],
-        [/\u200bತ/g, "ತ"], [/\u200bಥ/g, "ಥ"], [/\u200bದ/g, "ದ"], [/\u200bಧ/g, "ಧ"], [/\u200bನ/g, "ನ"],
-        [/\u200bಪ/g, "ಪ"], [/\u200bಫ/g, "ಫ"], [/\u200bಬ/g, "ಬ"], [/\u200bಭ/g, "ಭ"], [/\u200bಮ/g, "ಮ"],
-        [/\u200bಯ/g, "ಯ"], [/\u200bರ/g, "ರ"], [/\u200bಲ/g, "ಲ"], [/\u200bವ/g, "ವ"], [/\u200bಶ/g, "ಶ"],
-        [/\u200bಷ/g, "ಷ"], [/\u200bಸ/g, "ಸ"], [/\u200bಹ/g, "ಹ"], [/\u200bಳ/g, "ಳ"], [/\u200b /g, " "],
+        // === Anusvara ===
+        [/[MṃṂṁṀ]/g, "ಂ"],
+        [/​ಂ/g, "ಂ"],
+        [/್ಂ/g, "ಂ"],
 
-        // 6. Anusvara, Visarga, & Candrabindu
-        [/[MṃṂṁṀ]/g, "ಂ"],      // Type "M" for Anusvara
-        [/\u200bಂ/g, "ಂ"],      // Snaps Anusvara to the letter
+        // === Candrabindu ===
+        [/ಂಂ/g, "ಁ"],
+        [/ಁಂ/g, ""],
+        [/​ಁ/g, "ಁ"],
 
-        [/ಂಂ/g, "ಁ"],           // Type "MM" for Candrabindu
-        [/\u200bಁ/g, "ಁ"],      // Snaps Candrabindu to the letter
+        // === Visarga ===
+        [/[HḥḤ]/g, "ಃ"],
+        [/್ಃ/g, "ಃ"],
+        [/​ಃ/g, "ಃ"],
 
-        [/[HḥḤ]/g, "ಃ"],        // Type "H" for Visarga
-        [/\u200bಃ/g, "ಃ"],      // Snaps Visarga to the letter
+        // === Nukta ===
+        [/್=/g, "಼್"],
+        [/=/g, "಼"],
+        [/​಼/g, "಼"],
+
+        // === Cluster: zero-width joiner / non-joiner ===
+        [/x/g, "‍"],
+        [/್‍/g, "‍್"],
+        [/‍‍/g, "‌"],
+        [/‌್/g, "‌"],
 
         // ==========================================
-        // 7. VEDIC ACCENTS 
+        // VEDIC ACCENTS
         // ==========================================
-        [/_/g, "॒"],       // Type "_" for Anudatta
-        [/\u200b॒/g, "॒"],  // FIX: Removes invisible space before Anudatta
+        [/_/g, "॒"],             // Type "_" for Anudatta
+        [/​॒/g, "॒"],        // Remove invisible space before Anudatta
 
-        [/\+/g, "॑"],       // Type "+" for Swarita
-        [/\u200b॑/g, "॑"],  // FIX: Removes invisible space before Swarita
+        [/\+/g, "॑"],             // Type "+" for Swarita
+        [/​॑/g, "॑"],        // Remove invisible space before Swarita
 
-        [/॑॑/g, "᳚"],        // Type "++" for Dirgha Swarita
-        [/\u200b᳚/g, "᳚"],   // FIX: Removes invisible space before Dirgha Swarita
+        [/॑॑/g, "᳚"],             // Type "++" for Dirgha Swarita
+        [/​᳚/g, "᳚"],        // Remove invisible space before Dirgha Swarita
 
-        // 8. Punctuation & Extras
-        [/\'/g, "ಽ"],      // Avagraha
-        [/\|/g, "।"],      // Danda
+        // === Punctuation & extras ===
+        [/\'/g, "ಽ"],            // Avagraha
+        [/\|/g, "।"],            // Danda
         [/\//g, "।"],
-        [/।।/g, "॥"],     // Double Danda
-        [/x/g, "\u200d"],  // Zero width joiner for half consonants
-        [/\u200d\u200d/g, "\u200c"], // Zero width non-joiner
+        [/।।/g, "॥"],            // Double Danda
 
-        // 9. Numbers
+        // === Numbers ===
         [/0/g, "೦"], [/1/g, "೧"], [/2/g, "೨"], [/3/g, "೩"], [/4/g, "೪"],
         [/5/g, "೫"], [/6/g, "೬"], [/7/g, "೭"], [/8/g, "೮"], [/9/g, "೯"],
     ];
@@ -155,7 +170,7 @@
         return result;
     };
 
-    textarea.addEventListener("input", (e) => {
+    textarea.addEventListener("input", () => {
         const start = textarea.selectionStart;
         const end = textarea.selectionEnd;
         const scrollTop = textarea.scrollTop;
@@ -175,29 +190,23 @@
     });
 })();
 
-// Function to clean the text and copy it to clipboard
+// Clean the text and copy it to the clipboard
 function copy() {
     const textarea = document.getElementById("bar");
     let text = textarea.value;
 
     // Remove any leftover invisible Zero-Width Spaces before copying
-    const cleanReplacements = [
-        [/\u200b/g, ""] 
-    ];
-
-    for (const [regex, replacement] of cleanReplacements) {
-        text = text.replace(regex, replacement);
-    }
+    text = text.replace(/​/g, "");
 
     textarea.value = text;
-    
+
     // Copy to clipboard
     textarea.select();
     document.execCommand("copy");
-    
+
     // Show confirmation message
     const msg = document.getElementById("copymes");
-    if(msg) {
+    if (msg) {
         msg.style.display = "block";
         setTimeout(() => { msg.style.display = "none"; }, 2000);
     }
